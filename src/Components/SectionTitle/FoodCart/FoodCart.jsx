7429036bedbb477b3fc48bf92bebe4481817058f
Md Sabbir Khan <mdsabbirkhan1972@gmail.com>
2024-09-27 02:45:13 +0600

    second day done

--- src/Components/SectionTitle/FoodCart/FoodCart.jsx
+++ src/Components/SectionTitle/FoodCart/FoodCart.jsx
@@ -12,7 +12,7 @@ const FoodCart = ({ item }) => {
         <h2 className="card-title">{name}</h2>
         <p>{recipe}</p>
         <div className="card-actions justify-center">
-          <button className="btn btn-outline border-0 border-b-4 px-8 md:px-10 mt-5">
+          <button className="btn btn-outline border-0 border-b-4 border-orange-400 hover:border-orange-400 px-8 md:px-10 mt-5">
             Order Now
           </button>
         </div>
